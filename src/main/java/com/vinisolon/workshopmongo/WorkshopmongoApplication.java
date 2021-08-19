dc98ecde0e9cd53579f40b74ee8c8b372b30cbfb
Vinicius Solon <vini.97@live.com>
2021-08-19 19:54:54 -0300

    Primeiro commit - criação do projeto

---
+++ src/main/java/com/vinisolon/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.vinisolon.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
