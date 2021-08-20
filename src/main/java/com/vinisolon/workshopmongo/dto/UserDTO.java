8a070fc5f0bf532db914ed71612520bc7ddee70d
Vinicius Solon <vini.97@live.com>
2021-08-19 23:05:10 -0300

    Implementado DTO para retornar users

---
+++ src/main/java/com/vinisolon/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,27 @@
+package com.vinisolon.workshopmongo.dto;
+
+import com.vinisolon.workshopmongo.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private String email;
+
+    public UserDTO(User user) {
+        id = user.getId();
+        name = user.getName();
+        email = user.getEmail();
+    }
+
+}
--- src/main/java/com/vinisolon/workshopmongo/resources/UserResource.java
+++ src/main/java/com/vinisolon/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.vinisolon.workshopmongo.resources;
 
 import com.vinisolon.workshopmongo.domain.User;
+import com.vinisolon.workshopmongo.dto.UserDTO;
 import com.vinisolon.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,9 +20,10 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> users = service.findAll();
-        return ResponseEntity.ok().body(users);
+        List<UserDTO> usersDto = users.stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(usersDto);
     }
 
 }
