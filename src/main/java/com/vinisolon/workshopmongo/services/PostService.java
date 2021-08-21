0a4c8a3551db7ca8256214e97679e5e3ce6bfe22
Vinicius Solon <vini.97@live.com>
2021-08-21 10:00:59 -0300

    Recuperando Post pelo ID

--- src/main/java/com/vinisolon/workshopmongo/services/PostService.java
+++ src/main/java/com/vinisolon/workshopmongo/services/PostService.java
@@ -1,4 +1,22 @@
 package com.vinisolon.workshopmongo.services;
 
+import com.vinisolon.workshopmongo.domain.Post;
+import com.vinisolon.workshopmongo.repository.PostRepository;
+import com.vinisolon.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class PostService {
+
+    @Autowired
+    PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> post = repository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Post not found."));
+    }
+
 }
